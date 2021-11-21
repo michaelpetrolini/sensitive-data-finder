685a6eac80e6a9320bfcd1d983ba89ed90fbc7ec
PetroliniM <michael.petrolini@omigrade.it>
2021-11-21 11:50:53 +0100

    added statistics

--- data_preparation/pyspark_modeling.py
+++ data_preparation/pyspark_modeling.py
@@ -53,8 +53,8 @@ model = pipeline.fit(train_df)
 predictions = model.transform(test_df)
 
 
-predictionAndLabels = predictions.select('prediction', 'indexedLabel')\
-    .rdd.map(lambda x: (x['prediction'], x['indexedLabel']))
+# Overall statistics
+predictionAndLabels = predictions.rdd.map(lambda x: (x.prediction, x.indexedLabel))
 metrics = MulticlassMetrics(predictionAndLabels)
 precision = metrics.precision()
 recall = metrics.recall()
@@ -63,3 +63,17 @@ print("Summary Stats")
 print("Precision = %s" % precision)
 print("Recall = %s" % recall)
 print("F1 Score = %s" % f1Score)
+
+# Statistics by class
+labels = predictions.rdd.map(lambda x: (x.indexedLabel, x.label)).distinct().collect()
+for label in sorted(labels):
+    print("Class %s precision = %s" % (label[1], metrics.precision(label[0])))
+    print("Class %s recall = %s" % (label[1], metrics.recall(label[0])))
+    print("Class %s F1 Measure = %s" % (label[1], metrics.fMeasure(label[0], beta=1.0)))
+
+# Weighted stats
+print("Weighted recall = %s" % metrics.weightedRecall)
+print("Weighted precision = %s" % metrics.weightedPrecision)
+print("Weighted F(1) Score = %s" % metrics.weightedFMeasure())
+print("Weighted F(0.5) Score = %s" % metrics.weightedFMeasure(beta=0.5))
+print("Weighted false positive rate = %s" % metrics.weightedFalsePositiveRate)
